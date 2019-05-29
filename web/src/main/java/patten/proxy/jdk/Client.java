fb6468abd18de7e9e7461e8dfd93f3cd244ae23e
zhengzp <zhengzp@jiedaibao.com>
2019-05-29 11:28:16 +0800

    代理模式

--- web/src/main/java/patten/proxy/jdk/Client.java
+++ web/src/main/java/patten/proxy/jdk/Client.java
@@ -1,5 +1,8 @@
 package patten.proxy.jdk;
 
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
 import java.lang.reflect.Proxy;
 
 public class Client {
@@ -10,6 +13,17 @@ public class Client {
         MyInvocationHandler myInvocationHandler = new MyInvocationHandler(realSubject);
         //代理对象
         Subject proxyClass = (Subject) Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), new Class[]{Subject.class}, myInvocationHandler);
+
+        byte[] classFile = ProxyGenerator.generateProxyClass("$Proxy0", proxyClass.getClass().getInterfaces());
+        String path = "/Users/king/Data/workspace/jtest/web/src/main/java/patten/proxy/jdk/ProxySubject.class";
+        try{
+            FileOutputStream fos = new FileOutputStream(path);
+            fos.write(classFile);
+            fos.flush();
+            System.out.println("代理类class文件写入成功");
+        } catch (Exception e) {
+            System.out.println("写文件错误");
+        }
 
         proxyClass.sellBooks();
 
