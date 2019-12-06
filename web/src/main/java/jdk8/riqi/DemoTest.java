040a8bd6124355467d53f6276adeca02697043f5
zhengzp <zhengzp@jiedaibao.com>
2019-12-06 20:07:40 +0800

    fix est

---
+++ web/src/main/java/jdk8/riqi/DemoTest.java
@@ -0,0 +1,12 @@
+package jdk8.riqi;
+
+public class DemoTest {
+
+    public static void main(String[] args) {
+        Integer a = 12;
+        Integer b = 21;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+    }
+
+}
