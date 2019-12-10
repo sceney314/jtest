42e673b5a8078ca84c66f6e8e3ee1838dbae7fe3
邓凤娇 <lovelife@dengfengjiaodeMacBook-Pro.local>
2019-12-11 00:09:42 +0800

    最接近的三数之和

---
+++ web/src/main/java/leetcode/ThreeSum3.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * 较小的三数之和
+ *
+ * 给定一个长度为 n 的整数数组和一个目标值 target，寻找能够使条件 nums[i] + nums[j] + nums[k] < target 成立的三元组 i, j, k 个数（0 <= i < j < k < n）。
+ *
+ * 例如 nums = [-2, 0, 1, 3],  target = 2.
+ *
+ * 返回 2. 因为有两个三元组，其总和小于2:
+ *
+ * [-2, 0, 1]
+ * [-2, 0, 3]
+ *
+ * Date: 2019-12-11 00:08
+ * Copyright (C), 2015-2019
+ */
+public class ThreeSum3 {
+    public static void main(String[] args) {
+
+    }
+}
