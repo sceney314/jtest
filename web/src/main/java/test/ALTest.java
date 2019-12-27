ff922cd774bc0de7141faec57550766073221e54
zhengzp <zhengzp@jiedaibao.com>
2019-12-27 11:44:07 +0800

    fix 升级版本号

---
+++ web/src/main/java/test/ALTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import test.son.People;
+
+import java.util.ArrayList;
+
+/**
+ * @author zhengzp
+ * 2019-12-23 09:57
+ */
+public class ALTest {
+
+    public static void main(String[] args) {
+//        Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+//        System.out.println(DEFAULTCAPACITY_EMPTY_ELEMENTDATA.length);
+        ArrayList<People> peoples = new ArrayList<>();
+        peoples.add(new People("张三", 1));
+        peoples.add(1, new People("王五", 2));
+        peoples.contains(new People("李四", 3));
+
+        System.out.println(peoples);
+    }
+
+}
--- web/src/main/java/test/ExcelTest.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-/**
- * @author zhengzp
- * 2019-12-16 11:33
- */
-public class ExcelTest {
-}
