44fee05344c755c8a7c791128ba6a93ea16da672
zhengzp <zhengzp@jiedaibao.com>
2020-03-18 20:12:44 +0800

    fix 二分查找

--- web/src/main/java/leetcode/sortalgorithm/BinarySearch.java
+++ web/src/main/java/leetcode/sortalgorithm/BinarySearch.java
@@ -6,4 +6,25 @@ package leetcode.sortalgorithm;
  * 2020-03-17 07:09
  */
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 23, 34, 42, 63, 634, 5423};
+        System.out.println(binary(nums, 54231));
+    }
+
+    public static int binary(int[] nums, int target){
+        int left = 0, right = nums.length - 1;
+        int mid = (left + right) / 2;
+        while (left <= right){
+            if (nums[mid] < target){
+                left = mid + 1;
+            }else if(nums[mid] > target){
+                right = mid - 1;
+            }else {
+                return mid;
+            }
+            mid = (left + right) / 2;
+        }
+
+        return -1;
+    }
 }
