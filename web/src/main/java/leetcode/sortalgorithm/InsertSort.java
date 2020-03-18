25e912f080c1827c0232bc0ef74fb116daf91e48
zhengzp <zhengzp@jiedaibao.com>
2020-03-18 15:34:01 +0800

    fix 快速排序

--- web/src/main/java/leetcode/sortalgorithm/InsertSort.java
+++ web/src/main/java/leetcode/sortalgorithm/InsertSort.java
@@ -6,4 +6,26 @@ package leetcode.sortalgorithm;
  * 2020-03-17 07:05
  */
 public class InsertSort {
+    public static void main(String[] args) {
+        int[] nums = new int[]{34,5423,23, 5,634,42,63};
+        insertion(nums);
+        for (int n : nums){
+            System.out.printf("%d ", n);
+        }
+    }
+
+    public static void insertion(int[] nums){
+        for (int i = 1; i < nums.length; i++){
+            for (int k = i - 1, m = i; k >= 0; k--){
+                if (nums[m] >= nums[k]){
+                    break;
+                }
+
+                int tmp = nums[k];
+                nums[k] = nums[m];
+                nums[m] = tmp;
+                m = k;
+            }
+        }
+    }
 }
--- web/src/main/java/leetcode/sortalgorithm/QuickSort.java
+++ web/src/main/java/leetcode/sortalgorithm/QuickSort.java
@@ -1,9 +1,47 @@
 package leetcode.sortalgorithm;
 
 /**
- * 快速派去
+ * 快速排序
  * <p>
  * 2020-03-17 07:05
  */
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] nums = new int[]{34,5423,23, 5,34,634,42,63};
+        quick(nums, 0, nums.length - 1);
+        for (int n : nums){
+            System.out.printf("%d ", n);
+        }
+    }
+
+    public static void quick(int[] nums, int start, int end){
+        int pivot = nums[start];
+        int left = start, right = end;
+        while (left < right){
+            while (nums[right] > pivot){
+                right--;
+            }
+
+            while (nums[left] < pivot){
+                left++;
+            }
+
+            if (left < right && nums[left] == nums[right]){
+                left++;
+                continue;
+            }
+
+            int tmp = nums[right];
+            nums[right] = nums[left];
+            nums[left] = tmp;
+        }
+
+        if (left - 1 > start){
+            quick(nums, start, left - 1);
+        }
+
+        if (right + 1 < end){
+            quick(nums, right + 1, end);
+        }
+    }
 }
