5bed464e042460fdb596a7db03fae8b6a967639d
zhengzp <zhengzp@jiedaibao.com>
2020-03-18 19:44:29 +0800

    fix 希尔排序

--- web/src/main/java/leetcode/sortalgorithm/ShellSort.java
+++ web/src/main/java/leetcode/sortalgorithm/ShellSort.java
@@ -6,4 +6,27 @@ package leetcode.sortalgorithm;
  * 2020-03-17 07:08
  */
 public class ShellSort {
+    public static void main(String[] args) {
+        int[] nums = new int[]{34, 5423, 23, 5, 634, 42, 63};
+        shell(nums);
+        for (int n : nums) {
+            System.out.printf("%d ", n);
+        }
+    }
+
+    public static void shell(int[] nums) {
+        // 对数组进行分组, 直到 gap = 1分组完成，完成插入排序
+        for (int gap = nums.length / 2; gap > 0; gap /= 2) {
+            // 对分组进行插入排序
+            for (int k = gap; k < nums.length; k++) {
+                int m = k;
+                while (m - gap >= 0 && nums[m] < nums[m - gap]) {
+                    int tmp = nums[m];
+                    nums[m] = nums[m - gap];
+                    nums[m - gap] = tmp;
+                    m -= gap;
+                }
+            }
+        }
+    }
 }
