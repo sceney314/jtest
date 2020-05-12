3be3dca521fd91441c515bf7d20252a49a7d3c60
zhengzp <zhengzp@jiedaibao.com>
2020-05-12 20:31:24 +0800

    fix 忽略配置文件

--- web/src/main/java/test/CodeTest.java
+++ web/src/main/java/test/CodeTest.java
@@ -2,7 +2,10 @@ package test;
 
 
 import java.util.*;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 /**
@@ -15,6 +18,9 @@ public class CodeTest {
         map.put(1, "a");
         System.out.println(map);
 
+
+
+
         int HASH_BITS = 0x7fffffff;
         System.out.println(Integer.toBinaryString(HASH_BITS));
         System.out.println(Integer.toBinaryString(HASH_BITS).length());
@@ -23,7 +29,48 @@ public class CodeTest {
         String s = "outerapi/billing/statistics/";
         System.out.println(s.length());
 
+        ConcurrentLinkedQueue<Integer> queue = new ConcurrentLinkedQueue<>();
+        queue.offer(1);
+        queue.offer(2);
+        System.out.println(queue.toString());
+
+
+        Integer count = 0;
+        final List<Integer> nums = new ArrayList<>();
+        Callable<Integer> callable = Executors.callable(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("我就是跑跑");
+                nums.add(count);
+            }
+        }, count);
+
+        try {
+            System.out.println(callable.call());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        int idx = 0;
+        tset:
+        for (;;){
+            idx++;
+            System.out.println("进来啦" + idx + ", " + System.currentTimeMillis());
+            for (int i =0; ;i++){
+                if (i > 100){
+                    if (idx < 5){
+                        continue tset;
+                    }
+                    break tset;
+                }
+            }
+        }
+        System.out.println("跳出来啦");
+        System.out.println(Integer.toBinaryString(-1 << 29));
+        System.out.println(-1 << 29);
     }
+
 
     public static List<String> string2List(String content) {
         return string2List(content, ",");
@@ -35,7 +82,6 @@ public class CodeTest {
         }
         return Arrays.stream(content.split(separator)).collect(Collectors.toList());
     }
-
 
 
 }
